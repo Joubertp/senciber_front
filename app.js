7a961dab23a4c22b66f00cc21980d009caa037f6
dorinebertonTlse <dorineberton@gmail.com>
2021-09-28 16:09:27 +0200

    add scanner

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import data from './data.js'
 // generate a random qrcode
 const button = document.getElementById('random-btn');
 
-button.addEventListener('click', () => {      
+button.addEventListener('click', () => {
     const qrcode = document.getElementById('qrcode');
     qrcode.innerHTML = '';
 
@@ -12,31 +12,47 @@ button.addEventListener('click', () => {
         colorDark: "#000000",
         colorLight: "#ffffff",
         correctLevel: QRCode.CorrectLevel.H,
-      });
-      
+    });
 
-    const makeCode = () => {      
+
+    const makeCode = () => {
 
         const randomNumber = len => {
-        let random;
-        let n = '';
+            let random;
+            let n = '';
 
-        for(let count = 0; count < len; count++) {
-            random = Math.floor(Math.random() * 10);
-            n += random.toString();
+            for (let count = 0; count < len; count++) {
+                random = Math.floor(Math.random() * 10);
+                n += random.toString();
+            }
+            // console.log('n', n)
+            return n;
         }
-        // console.log('n', n)
-        return n;
-    }
 
-    let token = randomNumber(13);
-    data.token = token
-    console.log('data', data)
+        let token = randomNumber(13);
+        data.token = token
+        console.log('data', data)
 
-    // construct the url
-    const url = `https://google.com?token=${token}`
-    QR_CODE.makeCode(url);
+        // construct the url
+        const url = `https://google.com?token=${token}`
+        QR_CODE.makeCode(url);
     }
 
     makeCode();
 });
+
+
+let submit = document.getElementById('submit1')
+let mail, password;
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('e', e)
+    mail = document.getElementById('mail1').value
+    password = document.getElementById('password1').value
+    console.log('mail', mail, password)
+    data.mail = mail
+    data.password = password
+});
+
+
+console.log('data', data)
