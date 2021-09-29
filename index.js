fbdb3dc7d85c85eee19e693a715b0fd2b26b1a25
dorinebertonTlse <dorineberton@gmail.com>
2021-09-29 11:11:01 +0200

    fetch data

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const Port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next();
+})
+
 app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
+app.post("/register", (req, res) => {
+    // our register logic goes here...
+    console.log('test')
+    console.log(req)
+});
+
 server.listen(Port, () => { console.log('listening on *:3000'); });
 const { Server } = require("socket.io");
 const io = new Server(server);
